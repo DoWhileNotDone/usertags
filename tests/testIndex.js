a68be1ec66f0d00c262c20aa3f9fc4243c3662dd
Dave <davegthemighty@hotmail.com>
2019-11-16 13:17:46 +0000

    User Tags Service - Implement Tags
     * Add Manage Tags link to index page
     * Add unit test for manage tags link
     * Use supertest and node-html-parser for testing
     * Move index.pug to index sub directory

--- tests/testIndex.js
+++ tests/testIndex.js
@@ -2,23 +2,35 @@ process.env.NODE_ENV = 'test';
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const nodeHtmlParser  = require('node-html-parser');
+const request = require('supertest');
 
-const { expect } = chai;
 chai.use(chaiHttp);
+
+const { parse } = nodeHtmlParser;
+const { expect } = chai;
 
 const app = require("../app");
 
 describe("User Tags Service - Index", () => {
-  it("landing page for the api", done => {
-    chai
-      .request(app)
-      .get("/")
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res).to.be.html;
-        expect(res.text).to.include("S-Express");
-        expect(res.text).to.include("Welcome to S-Express");
-        done();
-      });
+
+  let page = null;
+  let response = null;
+
+  before(async function(){
+    response = await request(app).get("/");
+    page = parse(response.text);
+  })
+
+  it("landing page for the api", async function () {
+        expect(response).to.have.status(200);
+        expect(response).to.be.html;
+        expect(response.text).to.include("User Tag Service");
+        expect(response.text).to.include("Welcome to the User Tag Service");
+        //Parse Elements
+        const tagsLink = page.querySelector('#link-tags-manage');
+        expect(tagsLink).to.exist;
+        expect(tagsLink.text).to.equal("Manage Tags");
+        expect(tagsLink.attributes.href).to.equal("/tags/");
   });
 });
